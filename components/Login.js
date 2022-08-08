64f370095f91580decfc006f8bd7c7949be7c53a
CarlAbel <caweb.cph@gmail.com>
2022-08-08 18:23:09 +0200

    First commit

---
+++ components/Login.js
@@ -0,0 +1,26 @@
+import Head from "next/head"
+import { ChatIcon } from "@chakra-ui/icons"
+import { Box, Button, Center, Stack } from "@chakra-ui/react"
+
+export default function Login() {
+  return (
+    <>
+      <Head>
+        <title>Login</title>
+      </Head>
+
+      <Center h="100vh">
+        <Box
+          bgColor="blue.500"
+          w="fit-content"
+          p={5}
+          rounded="xl"
+          boxShadow="md"
+        >
+          <ChatIcon w="100px" h="100px" color="white" />
+        </Box>
+        <Button boxShadow="md">Sign In with Google</Button>
+      </Center>
+    </>
+  )
+}
